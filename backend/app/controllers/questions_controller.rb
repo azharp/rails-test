8cbfa22adc718f0bca58cb62a50c7e1ce50a1171
azrr <azharpratama@gmail.com>
2019-09-18 15:26:38 +0700

    actions for questions

--- backend/app/controllers/questions_controller.rb
+++ backend/app/controllers/questions_controller.rb
@@ -1,13 +1,35 @@
 class QuestionsController < ApplicationController
   # TODO: Please check out the test file to determine the methods you need to implement for this controller.
-
   def index
+    render json: Question.all.limit(3)
   end
 
   def show
+    id = params[:id]
+
+    begin
+      render json: Question.find(id)
+    rescue ActiveRecord::RecordNotFound
+      render :status => 404
+    end
   end
 
   def answer
+    begin
+      UserAnswer.create answer_param
+      render json: true
+    rescue ActiveRecord::RecordNotFound
+      render :status => 404
+    end
   end
 
+  private
+
+  def question
+    params.require(:question).permit(:id)
+  end
+
+  def answer_param
+    params.require(:answer).permit(:id, question: [:answer])
+  end
 end
